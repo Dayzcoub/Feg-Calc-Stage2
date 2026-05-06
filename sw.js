af29ff8cb3366e237d14fb1271106c00ea794833
Dayzcoub <77221495+Dayzcoub@users.noreply.github.com>
2026-05-06 23:04:54 +0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -35,9 +35,7 @@ self.addEventListener("fetch", event => {
       if (cached) return cached;
 
       return fetch(event.request).then(response => {
-        if (!response || response.status !== 200 || response.type !== "basic") {
-          return response;
-        }
+        if (!response || response.status !== 200 || response.type !== "basic") return response;
         const responseToCache = response.clone();
         caches.open(CACHE_NAME).then(cache => cache.put(event.request, responseToCache));
         return response;
